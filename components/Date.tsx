1fad82004365e1a9cb4659513d84e71bb347a53e
ykarakita <taf.749@gmail.com>
2023-11-03 11:05:15 +0900

    update date format

--- components/Date.tsx
+++ components/Date.tsx
@@ -2,5 +2,5 @@ import { format, parseISO } from 'date-fns'
 
 export const Date = (dateString: string): string => {
   const date = parseISO(dateString)
-  return format(date, 'MMMM dd, yyyy')
+  return format(date, 'yyyy-MM-dd')
 }
